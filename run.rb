a8b33ce5d9cc88ab7e4fe663897ed092232df746
Sebastian Jay <sebastianrjay@gmail.com>
2015-10-18 00:37:02 -0700

    Improve directions in run.rb

--- run.rb
+++ run.rb
@@ -1,10 +1,14 @@
 # Do not delete or comment this line:
 require_relative 'lib/heroku_app_pinger.rb'
 
-# Instantiate and run your app pingers in this file.
+# Instantiate and run your app pingers in this file, like so:
+#
+# HerokuAppPinger.new(url_name, utc_time_string_start, utc_time_string_end).run
 #
 # NOTE: all times must be written as 00:00 - 23:59, and are automatically
-# converted to UTC
+# converted to UTC. Integers represent hours with minutes set to :00. The
+# utc_time_string_end argument is optional. By default, HerokuAppPinger keeps
+# your app active for 17 hours, 58 minutes.
 #
 # Examples:
 #
